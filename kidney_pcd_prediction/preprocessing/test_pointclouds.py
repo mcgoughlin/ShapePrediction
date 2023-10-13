c1ee232016cf2de5821cf58981cd67e82905c5d0
William McGough <mcgoug01@f1td9j3.cri.camres.org>
2023-10-13 14:16:32 +0100

    Improved pointcloud test script - pointclouds appear correct

--- kidney_pcd_prediction/preprocessing/test_pointclouds.py
+++ kidney_pcd_prediction/preprocessing/test_pointclouds.py
@@ -8,28 +8,47 @@ path = '/media/mcgoug01/nvme/ThirdYear/CTORG_objdata/aligned_pointclouds'
 average_left_pointcloud = np.load(path + '/average_left.npy')
 average_right_pointcloud = np.load(path + '/average_right.npy')
 
-left_pointcloud = np.load(path + '/left/10.npy')
-right_pointcloud = np.load(path + '/right/10.npy')
+left_pointcloud1 = np.load(path + '/left/10.npy')
+left_pointcloud2 = np.load(path + '/left/11.npy')
+left_pointcloud23 = np.load(path + '/left/15.npy')
 
 # plot left point clouds and mark 3 random points with different colours that match between the two point clouds
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+#plot 3 pointclouds with the average pointcloud
 fig = plt.figure()
-ax = fig.add_subplot(111,projection='3d')
-ax.scatter(right_pointcloud[:,0],right_pointcloud[:,1],right_pointcloud[:,2],label='Right Pointcloud')
-ax.scatter(average_right_pointcloud[:,0],average_right_pointcloud[:,1],average_right_pointcloud[:,2],label='Average Right Pointcloud')
+#three subplots
+ax1 = fig.add_subplot(131, projection='3d')
+ax2 = fig.add_subplot(132, projection='3d')
+ax3 = fig.add_subplot(133, projection='3d')
 
-# pick 3 random points from the left pointcloud
-left_pointcloud_indices = np.random.choice(left_pointcloud.shape[0],3,replace=False)
+#remove space between subplots
+fig.subplots_adjust(wspace=0,hspace=0)
 
-# plot the 3 random points in the left pointcloud
-ax.scatter(right_pointcloud[left_pointcloud_indices,0],right_pointcloud[left_pointcloud_indices,1],right_pointcloud[left_pointcloud_indices,2],label='Left Pointcloud Indices',c='red',s=100)
-#plot the 3 random points in the average left pointcloud
-ax.scatter(average_right_pointcloud[left_pointcloud_indices,0],average_right_pointcloud[left_pointcloud_indices,1],average_right_pointcloud[left_pointcloud_indices,2],label='Average Left Pointcloud Indices',c='black',s=100)
-ax.set_title('Left Pointcloud')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+#plot the average pointcloud
+ax1.scatter(average_left_pointcloud[:,0],average_left_pointcloud[:,1],average_left_pointcloud[:,2],label='Average')
+#plot the first pointcloud
+ax1.scatter(left_pointcloud1[:,0],left_pointcloud1[:,1],left_pointcloud1[:,2],label='Pointcloud 1')
+
+#plot the average pointcloud
+ax2.scatter(average_left_pointcloud[:,0],average_left_pointcloud[:,1],average_left_pointcloud[:,2],label='Average')
+#plot the second pointcloud
+ax2.scatter(left_pointcloud2[:,0],left_pointcloud2[:,1],left_pointcloud2[:,2],label='Pointcloud 2')
+
+#plot the average pointcloud
+ax3.scatter(average_left_pointcloud[:,0],average_left_pointcloud[:,1],average_left_pointcloud[:,2],label='Average')
+#plot the third pointcloud
+ax3.scatter(left_pointcloud23[:,0],left_pointcloud23[:,1],left_pointcloud23[:,2],label='Pointcloud 3')
+
+# loop through the plots and highlight 3 random points in each plot
+random_point_indices = np.random.randint(0,200,3)
+#generate three different colours for each point
+colors = ['red','blue','green']
+for ax,pc in zip([ax1,ax2,ax3],[left_pointcloud1,left_pointcloud2,left_pointcloud23]):
+    ax.scatter(pc[random_point_indices,0],pc[random_point_indices,1],pc[random_point_indices,2],label='Random Points',color=colors,s=100)
+    #plot these points as black dots on the average pointcloud
+    ax.scatter(average_left_pointcloud[random_point_indices,0],average_left_pointcloud[random_point_indices,1],average_left_pointcloud[random_point_indices,2],label='Random Points',color='black',s=100)
+
 plt.show()
